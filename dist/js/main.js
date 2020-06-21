d4d35c48c1839c9380c1778723a58cf493e4ca52
Mtn-Drew <drewc3@gmail.com>
2020-06-21 15:43:11 -0600

    reverse menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,12 +6,26 @@ const menuNav = document.querySelector('.menu-nav')
 const menuBranding = document.querySelector('.menu-branding')
 const navItems = document.querySelectorAll('.nav-item')
 const homeBtn = document.querySelector('#home-when-home')
+const navLink = document.querySelector('.nav-link')
 
 //set initial state of menu
 let showMenu = false
 
+window.onload= setTimeout(reverseMenu,500)
+
 menuBtn.addEventListener('click', toggleMenu)
-homeBtn.addEventListener('click', toggleMenu)
+
+if(homeBtn){
+  homeBtn.addEventListener('click', toggleMenu)
+}
+
+function reverseMenu() {
+  menuBtn.classList.remove('close')
+  menu.classList.remove('show')
+  menuNav.classList.remove('show')
+  menuBranding.classList.remove('show')
+  navItems.forEach(item=> item.classList.remove('show'))
+}
 
 function toggleMenu() {
   if (!showMenu){
@@ -33,4 +47,69 @@ function toggleMenu() {
     //set menu state
     showMenu=false
   }
+}
+
+//project one
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const auto = false; // Auto scroll
+const intervalTime = 5000;
+let slideInterval;
+
+const nextSlide = () => {
+  // Get current class
+  const current = document.querySelector('.current');
+  // Remove current class
+  current.classList.remove('current');
+  // Check for next slide
+  if (current.nextElementSibling) {
+    // Add current to next sibling
+    current.nextElementSibling.classList.add('current');
+  } else {
+    // Add current to start
+    slides[0].classList.add('current');
+  }
+  setTimeout(() => current.classList.remove('current'));
+};
+
+const prevSlide = () => {
+  // Get current class
+  const current = document.querySelector('.current');
+  // Remove current class
+  current.classList.remove('current');
+  // Check for prev slide
+  if (current.previousElementSibling) {
+    // Add current to prev sibling
+    current.previousElementSibling.classList.add('current');
+  } else {
+    // Add current to last
+    slides[slides.length - 1].classList.add('current');
+  }
+  setTimeout(() => current.classList.remove('current'));
+};
+
+// Button events
+if (next) {
+  next.addEventListener('click', e => {
+  nextSlide();
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+})}
+
+if (prev) {
+  prev.addEventListener('click', e => {
+  prevSlide();
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+})}
+
+// Auto slide
+if (auto) {
+  // Run next slide at interval time
+  slideInterval = setInterval(nextSlide, intervalTime);
 }
