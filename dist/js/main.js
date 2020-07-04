d5f2dd6f19cb27cc9c587a8724dfb01cb1fca189
Mtn-Drew <drewc3@gmail.com>
2020-07-04 15:33:56 -0600

    project cards

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,11 +7,21 @@ const menuBranding = document.querySelector('.menu-branding')
 const navItems = document.querySelectorAll('.nav-item')
 const homeBtn = document.querySelector('#home-when-home')
 const navLink = document.querySelector('.nav-link')
+
 const pwGithubLink = document.querySelector('.pw-github-link')
 const gmGithubLink = document.querySelector('.gm-github-link')
 const scGithubLink = document.querySelector('.sc-github-link')
 const scApiGithubLink = document.querySelector('.sc-api-github-link')
 const sgGithubLink = document.querySelector('.sg-github-link')
+
+const gmCard = document.querySelector('#gm')
+const gmDesc = document.querySelector('#gm-desc')
+const scCard = document.querySelector('#sc')
+const scDesc = document.querySelector('#sc-desc')
+const sgCard = document.querySelector('#sg')
+const sgDesc = document.querySelector('#sg-desc')
+const pwCard = document.querySelector('#pw')
+const pwDesc = document.querySelector('#pw-desc')
 
 //set initial state of menu
 let showMenu = false
@@ -137,5 +147,40 @@ scApiGithubLink.addEventListener('click', (e)=>{
 sgGithubLink.addEventListener('click', (e)=>{
   window.open("https://github.com/Mtn-Drew/grocery",'_blank')
   e.stopPropagation()
+})
+
+const addReveal = (el) =>{
+  el.classList.add('text-revealed')
+}
+
+// Paragraph descriptions
+gmCard.addEventListener('mouseenter', ()=>{
+  gmDesc.classList.remove('text-hidden')
+  addReveal(gmDesc)
+
+})
+gmCard.addEventListener('mouseleave', ()=>{
+  gmDesc.classList.add('text-hidden')
+})
+
+scCard.addEventListener('mouseenter', ()=>{
+  scDesc.classList.remove('text-hidden')
+})
+scCard.addEventListener('mouseleave', ()=>{
+  scDesc.classList.add('text-hidden')
+})
+
+sgCard.addEventListener('mouseenter', ()=>{
+  sgDesc.classList.remove('text-hidden')
+})
+sgCard.addEventListener('mouseleave', ()=>{
+  sgDesc.classList.add('text-hidden')
+})
+
+pwCard.addEventListener('mouseenter', ()=>{
+  pwDesc.classList.remove('text-hidden')
+})
+pwCard.addEventListener('mouseleave', ()=>{
+  pwDesc.classList.add('text-hidden')
 })
 
